a4b7956789226c894d4a0ff3c1710fe2fab70982
stellar-game-7748 <danbruch@gmail.com>
2019-01-09 21:42:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,17 @@ function whisper(string) {
   return string.toLowerCase();
 }
 
+function logShout(string) {
+  console.log(string.toUpperCase());
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase());
+}
+
+function sayHiToGrandma(string) {
+  if (string === 'I love you, Grandma.') {
+    return 'I love you, too';
+  }
+  return 'YES INDEED!';
+}
